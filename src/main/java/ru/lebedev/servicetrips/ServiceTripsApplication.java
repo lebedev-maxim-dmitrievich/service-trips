0e5e3aacfe44c6cf11fec311fb473b897ba04954
lebedev-maxim-dmitrievich <  lebedevmaximdmitrievich@gmail.com>
2021-05-13 11:50:12 +0300

    inti commit

---
+++ src/main/java/ru/lebedev/servicetrips/ServiceTripsApplication.java
@@ -0,0 +1,13 @@
+package ru.lebedev.servicetrips;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceTripsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceTripsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/lebedev/servicetrips/ServiceTripsApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.lebedev.servicetrips;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceTripsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
